8e246fd2c8703ce263957627dab8d47cee423e4f
6299352U <umamageshwaran.natarajan@gmail.com>
2024-10-18 06:27:43 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,11 +1,3 @@
 DECLARE @tenant1 nvarchar(30) = 'SynapseTest6'
 EXECUTE [dbo].[createcustomerschema1]
 	@tenant = @tenant1
-
-CREATE TABLE @tenant1.Persons (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
-);
