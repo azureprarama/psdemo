22dfe0ddfe196488ffb65facb4f6c7a0e2da25a5
6299352U <umamageshwaran.natarajan@gmail.com>
2024-10-18 06:28:26 +0100

    Add files via upload

---
+++ schema2.sql
@@ -0,0 +1,9 @@
+DECLARE @tenant1 nvarchar(30) = 'SynapseTest6'
+
+CREATE TABLE @tenant1.Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
