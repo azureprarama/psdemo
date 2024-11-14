4d5094943269dbfc2801e31dfa504091112d466b
6299352U <umamageshwaran.natarajan@gmail.com>
2024-11-14 21:39:52 +0000

    Update listblob.ps1

--- listblob.ps1
+++ listblob.ps1
@@ -1,5 +1,3 @@
-$env:PSModulePath=$env:PSModulePath+";"+"C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell"
-Import-Module Az
 $Context = New-AzStorageContext -StorageAccountName spoolstoragetest -UseConnectedAccount
 # Get a list of containers in a storage account
 Get-AzStorageContainer -context $context
