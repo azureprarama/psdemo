8ed3a4ff844633487df96e8838cb2166e56d5461
6299352U <umamageshwaran.natarajan@gmail.com>
2024-11-14 15:48:56 +0000

    Update listblob.ps1

--- listblob.ps1
+++ listblob.ps1
@@ -1,1 +1,8 @@
-Get-AzStorageAccount | Select-Object StorageAccountName
+$azStorageAccountName = "" # Name of your storage account 
+$azStorageAccountKey = "" # Access key for your storage account
+$azContainerName = "" # Container name to list your blobs
+$azResourceGroupName = "" # Resource group name where storage account lives
+
+$Context = New-AzStorageContext -StorageAccountName $row.StorageAccountName -UseConnectedAccount
+# Get a list of containers in a storage account
+Get-AzStorageContainer -context $context
